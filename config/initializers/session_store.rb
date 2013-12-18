45c952eb32e8b926f05a9fe95154e5a677564193
afterburnertech <brandon@afterburnertechnologies.com>
2013-12-18 14:56:50 -0500

    MWR 4.4

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
 #WarBoards::Application.config.session_store :cookie_store, key: '_war_boards_session'
-WarBoards::Application.config.session_store :cache_store, key: '_war_boards_session'
+#WarBoards::Application.config.session_store :cache_store, key: '_war_boards_session'
 
 #WarBoards::Application.config.session_store :active_record_store
+
+# We’ve added the domain option here which will scope the 
+# session to the domain-level, rather than the subdomain level. 
+if Rails.env.test? WarBoards::Application.config.session_store :cache_store,
+    key: '_war_boards_session',
+	domain: 'example.com' 
+else
+  WarBoards::Application.config.session_store :cookie_store,
+    key: '_war_boards_session',
+    domain: 'foremapp.com'
+end
 
